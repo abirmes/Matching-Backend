86f4057f7679e7ec3c7815e420ea09216d9fa61e
abirmes <abir.meskini02@gmail.com>
2025-05-07 10:59:32 +0100

    last commit

--- app/Enums/CentreSpecialite.php
+++ app/Enums/CentreSpecialite.php
@@ -15,3 +15,14 @@ enum CentreSpecialite: string
     case LANGUAGE = 'language';
     case OTHER = 'other';
 }
+
+case SPORTS = '';
+    case MUSIC = 'music';
+    case ARTS = 'arts';
+    case TECHNOLOGY = 'technology';
+    case EDUCATION = 'education';
+    case FITNESS = 'fitness';
+    case DANCE = 'dance';
+    case COOKING = 'cooking';
+    case LANGUAGE = 'language';
+    case OTHER = 'other';
